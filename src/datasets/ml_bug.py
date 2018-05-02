6d9a013a9f3427532c30e7441e2575cde09d9a60
Mario Guerriero <marioguerriero33@gmail.com>
2018-05-02 11:05:05 +0200

    update slides and bug report script

--- src/datasets/ml_bug.py
+++ src/datasets/ml_bug.py
@@ -6,18 +6,18 @@ duplicatedCheck = moodyLyricsDF.groupby(['Artist','Song']).agg('count')
 duplicatedRows = duplicatedCheck [duplicatedCheck['Index']>1]
 duplicatedRows.reset_index(level=['Artist','Song'], inplace=True)
 
-print(moodyLyricsDF.head(2))
-
 rows = list()
 
 for (idx, row) in duplicatedRows.iterrows():
     artist, song = row['Artist'], row['Song']
     emotions = moodyLyricsDF[(moodyLyricsDF['Artist'] == artist) & (moodyLyricsDF['Song'] == song)]['Emotion']
     emotions = emotions.as_matrix().tolist()
+    indexes = moodyLyricsDF[(moodyLyricsDF['Artist'] == artist) & (moodyLyricsDF['Song'] == song)]['Index']
+    indexes = indexes.as_matrix().tolist()
     rows.append((
-        artist, song, row['Index'], emotions
+        artist, song, row['Index'], emotions, indexes
     ))
 
-bugReport = pd.DataFrame(rows, columns=['Artist', 'Song', 'Duplicate_Count', 'Emotions'])
+bugReport = pd.DataFrame(rows, columns=['Artist', 'Song', 'Duplicate_Count', 'Emotions', 'MooyLyrics_Indexes'])
 
 bugReport.to_csv('moodylyrics_bug_report.csv', index=False)
