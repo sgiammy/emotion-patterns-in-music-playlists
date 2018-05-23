e4f9cd5a5b717f9743c8ccea73dd3e3789b09a46
sgiammy <sgiammy@gmail.com>
2018-05-23 17:18:07 +0200

    Almost done

---
+++ src/datasets/recsys_playlists_emotion_classification.py
@@ -0,0 +1,126 @@
+import spacy
+import os
+import sys
+import json
+import itertools
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from utils.datasets import load_dataset_from_path, split_train_validation
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from utils.dataset_parsing import *
+from utils.progress import progress
+import utils.sentiment_analysis as sa
+import lyricwikia
+import io
+from utils.spotify_utils import * 
+
+'''
+We first read the json files extracted from the Spotify dataset
+We aim to: 
+	1. Read the json files and save into a pandas dataframe the following information: 
+	   <PlaylistPid, PlaylistName, TrackUri, ArtistName, TrackName>
+	2. Download the song lyrics and featurize the created dataset following the features selection described in the previous notebook
+	3. Train our classificator with the MoodyLyrics dataset (4 emotions: happy, sad, angry, relaxed)
+	4. Predict the emotion of each track in the spotify dataframe and add 4 columns: 
+	   <PlaylistPid, PlaylistName, TrackUri, ArtistName, TrackName, Happy_CL, Sad_CL, Angry_CL, Relaxed_CL>
+	   where CL stands for confidence level. 
+	5. Once we classified each song in a playlist, we sum up the rows for each emotion confidence level.
+	   We then create a new dataframe: 
+	   <PlaylistPid, PlaylistName,Happy_CL, Sad_CL, Angry_CL, Relaxed_CL>
+
+'''
+
+'''
+PART 1: Read the json files and save into a pandas dataframe the following information: 
+	   <PlaylistPid, PlaylistName, TrackUri, ArtistName, TrackName>
+'''
+df = read_spotify_json('../datasets/spotify_slice_prova.json')
+'''
+END PART 1 
+'''
+
+
+'''
+PART 2:	Download the song lyrics and featurize the created dataset following the features 
+		selection described in the previous notebook
+'''
+lyrics_dir = './spotify_lyrics'
+output_path = 'datasets/Spotify1stSlice_featurized.csv'
+new_df = download_and_featurize(df, lyrics_dir,output_path)
+'''
+END PART 2
+'''
+
+'''
+PART 3: Train our classificator with the MoodyLyrics dataset (4 emotions: happy, sad, angry, relaxed)
+'''
+# MoodyLyrics Preprocessing
+dataset = pd.read_csv('./datasets/moodylyrics_featurized.csv')
+selected_columns = [
+   'LYRICS_VECTOR',
+   'WORD_COUNT', 'ECHOISMS', 'SELFISH_DEGREE', 
+   'DUPLICATE_LINES', 'IS_TITLE_IN_LYRICS', 'VERB_PRESENT', 
+   'VERB_PAST', 'VERB_FUTURE', 'ADJ_FREQUENCIES',
+   'PUNCT_FREQUENCIES','SENTIMENT','SUBJECTIVITY',
+   'EMOTION'
+]
+dataset = dataset[selected_columns]
+train_df = dataset.drop(['EMOTION'], axis=1)
+# X and Y vect creation
+X_vect = adjust(train_df)
+y = dataset['EMOTION'].as_matrix().T
+# Train,test split
+X_train, X_test, y_train, y_test = train_test_split(X_vect, y, test_size = 0.2, random_state = 0)
+# Neural network: model definition, training,prediction and validation
+classifier,sc,accuracy,encoder = train_and_predict(X_train, X_test, y_train, y_test)
+print('Accuracy: %0.2f' % (accuracy*100))
+
+'''
+PART 4:  Predict the emotion of each track in the spotify dataframe and add 4 columns: 
+	   <PlaylistPid, PlaylistName, TrackUri, ArtistName, TrackName, Happy_CL, Sad_CL, Angry_CL, Relaxed_CL>
+	   where CL stands for confidence level. 
+'''
+new_df = pd.read_csv('./datasets/spotify_lyrics_1st.csv')
+selected_columns = ['PlaylistPid', 'PlaylistName', 'TrackUri', 'ArtistName', 'TrackName',
+       'LyricVector', 'wordCount', 'Echoisms', 'Selfish',
+       'DuplicateLines', 'IsTitleInLyrics', 'VerbPresent',
+       'VerbPast', 'VerbFuture', 'ADJ', 'PUNCT'
+]
+
+tmp_df = new_df[selected_columns]
+tmp_df = tmp_df.drop(['PlaylistPid', 'PlaylistName', 'TrackUri', 'ArtistName', 'TrackName'], axis=1)
+
+
+X_vect = adjust(tmp_df)
+X_vect_scaled = sc.transform(X_vect)
+y_pred = classifier.predict(X_vect_scaled,verbose=0)
+#y_pred_label = [list(zip(y_pred[i], emotions_label)) for i in range(len(y_pred))]
+#y_pred_ord = [sorted(y_pred_label[i], key=lambda x:  x[0], reverse=True) for i in range(len(y_pred_label))]
+emotion_labels = encoder.inverse_transform([0,1,2,3])
+classificationDf = pd.DataFrame(data=y_pred,columns=emotion_labels)
+finalDf = pd.concat([new_df, classificationDf],axis=1)
+output_path = './datasets/spotify_lyrics1st_classified'
+finalDf.to_csv(output_path,index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
